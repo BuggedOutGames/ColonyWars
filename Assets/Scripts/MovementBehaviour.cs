1b201a4fc4c21d87a503cdd6fca5ab1995fefb73
tylerlaberge <tylerlaberge@protonmail.com>
2020-11-04 19:25:02 -0500

    Added movement animation and movement rotation

--- Assets/Scripts/MovementBehaviour.cs
+++ Assets/Scripts/MovementBehaviour.cs
@@ -2,10 +2,24 @@
 
 public class MovementBehaviour : MonoBehaviour {
 
+    private static readonly int Walking = Animator.StringToHash("walking");
+
+    [Range(1, 10)]
     public float movementSpeed;
+    [Range(1, 10)]
+    public float rotationSpeed;
+
+    private Rigidbody2D rigidBody;
+    private Animator animator;
     private Vector2? moveDestination;
+    private float adjustedMoveSpeed;
+    private float adjustedRotationSpeed;
     
     private void Start() {
+        adjustedMoveSpeed = movementSpeed / 2;
+        adjustedRotationSpeed = rotationSpeed * 30;
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         EventManager.Instance.MoveCommandEvent += HandleMoveCommandEvent;
     }
 
@@ -13,23 +27,38 @@ public class MovementBehaviour : MonoBehaviour {
         EventManager.Instance.MoveCommandEvent -= HandleMoveCommandEvent;
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         if (moveDestination.HasValue) {
-            MoveTowards(moveDestination.Value, movementSpeed * Time.deltaTime);
+            MoveTowards(moveDestination.Value);
         }
     }
 
     private void HandleMoveCommandEvent(object sender, MoveCommand moveCommand) {
         if (moveCommand.Unit.gameObject.Equals(gameObject)) {
             moveDestination = moveCommand.Destination;
+            StartWalkAnimation();
         }
     }
 
-    private void MoveTowards(Vector2 target, float distance) {
-        Vector2 newPosition = Vector2.MoveTowards(transform.position, target, distance);
-        gameObject.GetComponent<Rigidbody2D>().MovePosition(newPosition);
-        if (Vector2.Distance(transform.position, target) < 1) {
+    private void MoveTowards(Vector2 target) {
+        RotateTowards(target);
+        MoveForward();
+        if (Vector2.Distance(transform.position, target) < 0.1) {
             moveDestination = null;
+            StopWalkAnimation();
         }
     }
+
+    private void RotateTowards(Vector2 target) {
+        Vector2 direction = (target - (Vector2) transform.position).normalized;
+        Quaternion newRotation = Quaternion.LookRotation(Vector3.forward, direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, adjustedRotationSpeed * Time.deltaTime);
+    }
+
+    private void MoveForward() {
+        rigidBody.MovePosition(transform.position + transform.up * (adjustedMoveSpeed * Time.deltaTime));
+    }
+
+    private void StartWalkAnimation() => animator.SetBool(Walking, true);
+    private void StopWalkAnimation() => animator.SetBool(Walking, false);
 }
